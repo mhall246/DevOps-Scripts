3c6eede665c7408089f3fd98dd7d0fa7d830a145
Matthew Hall <matthew.hall@halltechonline.com>
2018-02-26 15:15:13 -0500

    Add files via upload

---
+++ proxymonitor.ps1
@@ -0,0 +1,7 @@
+﻿$proxy = [System.Net.WebProxy]::GetDefaultProxy() | select address | Select -ExpandProperty Address | Select -ExpandProperty OriginalString
+
+IF([string]::IsNullOrEmpty($proxy)) {            
+    Write-Output "No Proxy Configured" | out-file -filepath "C:\Program Files\New Relic\plugins\proxymonitor\proxy.txt" -Encoding Ascii         
+} else {            
+    Write-Output "$proxy" | out-file -filepath "C:\Program Files\New Relic\plugins\proxymonitor\proxy.txt" -Encoding Ascii            
+}
